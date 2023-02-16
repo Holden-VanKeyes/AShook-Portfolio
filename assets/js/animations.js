a34380481f96bb06fe8d3d82ce2b8aef82e22867
Holden-VanKeyes <102389369+Holden-VanKeyes@users.noreply.github.com>
2023-02-16 14:14:40 -0600

    added videos

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,7 +1,10 @@
 const logo = document.querySelector('#bulb')
 const bgColor = document.querySelector('div.logo')
+
 const white = 'rgb(255, 255, 255)'
 const yellow = 'rgb(247, 239, 153)'
+const bulb = document.querySelector('i.fa')
+bulb.addEventListener('click', handleClick)
 
 function colorChange() {
   logo.style.color = yellow
@@ -21,3 +24,13 @@ function flasher() {
     bgColor.style.backgroundColor = 'black'
   }
 }
+
+function handleClick() {
+  if (bulb.className === 'fa fa-lightbulb fa-8x fa-spin') {
+    bulb.className = 'fa fa-lightbulb fa-8x'
+  } else bulb.className = 'fa fa-lightbulb fa-8x fa-spin'
+}
+
+// This is <b>bold</b> and this is <strong>strong</strong>. This is <i>italic</i> and this is <em>emphasized</em>.
+// 									This is <sup>superscript</sup> text and this is <sub>subscript</sub> text.
+// 									This is <u>underlined</u> and this is code: <code>for (;;) { ... }</code>. Finally, <a href="#">this is a link</a>.
